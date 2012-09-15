3d378e845077cc5de924409f37d51f1aadebfaba
Malgorzata Jatczyk <guska076@webdev20.pl>
2012-09-15 10:20:56 +0200

    bazowa wersja z gornym paskiem

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
-	var lat, lon, w, h, mq, beaches;
+	var lat, lon, w, h, mq, pano_options, map_options, mapa;
+	var mapDiv = document.getElementById('map');
+	
 	// check for Geolocation support
 	function getLocation(callback)
 	{
@@ -11,17 +13,27 @@ $(document).ready(function() {
 			});
 		}
 		else {
+			lat = 49.299181;
+			lon = 19.9495621;
+			callback();
 			//console.log('Geolocation is not supported for this Browser/OS version yet.');
+			
 		}
 		
 	}
+	
 	//var lat = -33.8669710, lon = 151.1958750, w, h;
 	getLocation(initApp);
 	function initApp()
 	{
 		
-		beaches = {'rect': {'sw': {'lat': lat-0.002, 'lng': lon-0.002}, 'ne': {'lat': lat+0.002, 'lng': lon+0.002}}};
-		
+		pano_options = {'rect': {'sw': {'lat': lat-0.002, 'lng': lon-0.002}, 'ne': {'lat': lat+0.002, 'lng': lon+0.002}}};
+		map_options = {
+			center: new google.maps.LatLng(lat, lon),
+			zoom: 14,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		mapa = new google.maps.Map(mapDiv, map_options);
 		// media query event handler
 		if (matchMedia) {
 			var mq = window.matchMedia("(min-width: 800px)");
@@ -31,7 +43,7 @@ $(document).ready(function() {
 				WidthChange(mq);
 				/* Refresh widget
 				var photo_ex_options = {'width': w, 'height': h};
-				var photo_ex_widget = new panoramio.PhotoWidget('pano_photos', beaches, photo_ex_options);
+				var photo_ex_widget = new panoramio.PhotoWidget('pano_photos', pano_options, photo_ex_options);
 				photo_ex_widget.setPosition(0);
 				*/
 			});
@@ -39,7 +51,7 @@ $(document).ready(function() {
 		
 		//init panoramio widget
 		var photo_ex_options = {'width': w, 'height': h};
-		var photo_ex_widget = new panoramio.PhotoWidget('pano_photos', beaches, photo_ex_options);
+		var photo_ex_widget = new panoramio.PhotoWidget('pano_photos', pano_options, photo_ex_options);
 		photo_ex_widget.setPosition(0);
 	}
 	
